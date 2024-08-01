fb99401b78e8c9ced05fa493ea066d6f925adbcb
rtthw <172864013+rtthw@users.noreply.github.com>
2024-08-01 14:34:32 -0400

    ...

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
-use yew::prelude::*;
 
-
-
-const HOME_HTML: &str = include_str!("home.html");
+use yew::{
+    html::Scope,
+    prelude::*,
+};
+use yew_router::prelude::*;
+use rtthw::{
+    home::*,
+    page_not_found::*,
+};
 
 
 
@@ -11,7 +16,7 @@ const HOME_HTML: &str = include_str!("home.html");
 
 
 fn main() {
-    yew::Renderer::<Index>::new().render();
+    yew::Renderer::<App>::new().render();
 }
 
 
@@ -20,18 +25,126 @@ fn main() {
 
 
 
-struct Index;
+struct App {
+    navbar_active: bool,
+}
 
-impl Component for Index {
-    type Message = ();
+impl Component for App {
+    type Message = Msg;
     type Properties = ();
 
     fn create(_ctx: &Context<Self>) -> Self {
-        Self
+        Self {
+            navbar_active: false,
+        }
     }
 
-    fn view(&self, _ctx: &Context<Self>) -> Html {
-        Html::from_html_unchecked(HOME_HTML.into())
+    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
+        match msg {
+            Msg::ToggleNavbar => {
+                self.navbar_active = !self.navbar_active;
+                true
+            }
+        }
+    }
+
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        html! {
+            <BrowserRouter>
+                { self.view_nav(ctx.link()) }
+
+                <main>
+                    <Switch<Route> render={switch} />
+                </main>
+                <footer class="footer">
+                    <div class="content has-text-centered">
+                        { "Powered by " }
+                        <a href="https://yew.rs">{ "Yew" }</a>
+                        { " using " }
+                        <a href="https://bulma.io">{ "Bulma" }</a>
+                        { " and images from " }
+                        <a href="https://unsplash.com">{ "Unsplash" }</a>
+                    </div>
+                </footer>
+            </BrowserRouter>
+        }
+    }
+}
+
+impl App {
+    fn view_nav(&self, link: &Scope<Self>) -> Html {
+        let Self { navbar_active, .. } = *self;
+
+        let active_class = if !navbar_active { "is-active" } else { "" };
+
+        html! {
+            <nav class="navbar is-primary" role="navigation" aria-label="main navigation">
+                <div class="navbar-brand">
+                    <h1 class="navbar-item is-size-3">{ "Yew Blog" }</h1>
+
+                    <button class={classes!("navbar-burger", "burger", active_class)}
+                        aria-label="menu" aria-expanded="false"
+                        onclick={link.callback(|_| Msg::ToggleNavbar)}
+                    >
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                    </button>
+                </div>
+                <div class={classes!("navbar-menu", active_class)}>
+                    <div class="navbar-start">
+                        <Link<Route> classes={classes!("navbar-item")} to={Route::Home}>
+                            { "Home" }
+                        </Link<Route>>
+                        <Link<Route> classes={classes!("navbar-item")} to={Route::Home}>
+                            { "Other" }
+                        </Link<Route>>
+
+                        <div class="navbar-item has-dropdown is-hoverable">
+                            <div class="navbar-link">
+                                { "More" }
+                            </div>
+                            <div class="navbar-dropdown">
+                                <Link<Route> classes={classes!("navbar-item")} to={Route::Home}>
+                                    { "Back Home" }
+                                </Link<Route>>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        }
+    }
+}
+
+pub enum Msg {
+    ToggleNavbar,
+}
+
+
+
+// ================================================================================================
+
+
+
+#[derive(Routable, PartialEq, Eq, Clone, Debug)]
+pub enum Route {
+    #[at("/")]
+    Home,
+    #[not_found]
+    #[at("/404")]
+    NotFound,
+}
+
+
+fn switch(routes: Route) -> Html {
+    match routes {
+        Route::Home => {
+            html! { <Home /> }
+        }
+        Route::NotFound => {
+            html! { <PageNotFound /> }
+        }
     }
 }
 
