623a90fc05c3e4abad28d90391516ef190e7fb92
rtthw <172864013+rtthw@users.noreply.github.com>
2024-08-04 13:56:06 -0400

    ...

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,30 @@
 
-pub mod about;
-pub mod home;
-pub mod page_not_found;
 
-pub use about::*;
-pub use home::*;
-pub use page_not_found::*;
+
+use eframe::egui;
+
+
+
+// ================================================================================================
+
+
+
+pub struct App {}
+
+impl eframe::App for App {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.centered_and_justified(|ui| ui.heading("Nothing here, yet..."));
+        });
+    }
+}
+
+impl App {
+    pub fn new(_cc: &eframe::CreationContext) -> Self {
+        Self {}
+    }
+}
+
+
+
+// ================================================================================================
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
+//! Personal Website
 
-use yew::prelude::*;
-use yew_router::prelude::*;
-use rtthw::*;
+
+
+use eframe::egui;
 
 
 
@@ -9,17 +10,51 @@ use rtthw::*;
 
 
 
+#[cfg(not(target_arch = "wasm32"))]
+fn main() -> eframe::Result {
+    eframe::run_native(
+        "rtthw",
+        eframe::NativeOptions {
+            viewport: egui::ViewportBuilder::default()
+                .with_inner_size([1280.0, 720.0])
+                .with_min_inner_size([1280.0, 720.0]),
+            ..Default::default()
+        },
+        Box::new(|cc| Ok(Box::new(rtthw::App::new(cc)))),
+    )
+}
+
+#[cfg(target_arch = "wasm32")]
 fn main() {
-    yew::Renderer::<App>::new().render();
-}
+    let web_options = eframe::WebOptions::default();
 
-#[function_component]
-pub fn App() -> Html {
-    html! {
-        <BrowserRouter>
-            <Switch<Route> render={switch} />
-        </BrowserRouter>
-    }
+    wasm_bindgen_futures::spawn_local(async {
+        let start_result = eframe::WebRunner::new()
+            .start(
+                "the_canvas_id",
+                web_options,
+                Box::new(|cc| Ok(Box::new(eframe_template::TemplateApp::new(cc)))),
+            )
+            .await;
+
+        // Remove the loading text and spinner:
+        let loading_text = web_sys::window()
+            .and_then(|w| w.document())
+            .and_then(|d| d.get_element_by_id("loading_text"));
+        if let Some(loading_text) = loading_text {
+            match start_result {
+                Ok(_) => {
+                    loading_text.remove();
+                }
+                Err(e) => {
+                    loading_text.set_inner_html(
+                        "<p> The app has crashed. Sorry, I guess? </p>",
+                    );
+                    panic!("Failed to start eframe: {e:?}");
+                }
+            }
+        }
+    });
 }
 
 
@@ -28,31 +63,7 @@ pub fn App() -> Html {
 
 
 
-#[derive(Routable, PartialEq, Eq, Clone, Debug)]
-pub enum Route {
-    #[at("/")]
-    Home,
-    #[not_found]
-    #[at("/404")]
-    NotFound,
-    #[at("/about")]
-    About,
-}
 
-
-fn switch(routes: Route) -> Html {
-    match routes {
-        Route::Home => {
-            html! { <Home /> }
-        }
-        Route::NotFound => {
-            html! { <PageNotFound /> }
-        }
-        Route::About => {
-            html! { <About /> }
-        }
-    }
-}
 
 
 
