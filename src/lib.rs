030db59bdc58fd2db8f6c33a0cc7d74dd6a82ef6
rtthw <172864013+rtthw@users.noreply.github.com>
2024-08-04 15:00:59 -0400

    ...

--- src/lib.rs
+++ src/lib.rs
@@ -12,9 +12,17 @@ use eframe::egui;
 pub struct App {}
 
 impl eframe::App for App {
+    #[cfg(not(target_arch = "wasm32"))]
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.centered_and_justified(|ui| ui.heading("Nothing here, yet..."));
+        });
+    }
+
+    #[cfg(target_arch = "wasm32")]
+    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.centered_and_justified(|ui| ui.heading(&frame.info().web_info.location.url));
         });
     }
 }
