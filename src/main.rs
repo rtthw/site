5b8227095c8b00558fa44df7303f7d0179292e9d
rtthw <172864013+rtthw@users.noreply.github.com>
2024-08-05 13:34:01 -0400

    ...

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,48 @@
 use leptos::*;
+use leptos_meta::*;
+use leptos_router::*;
+
+
 
 fn main() {
-    mount_to_body(|| {
-        view! {
-            <App increment=5 />
-        }
-    })
+    mount_to_body(|| view! { <App /> })
+}
+
+
+
+#[component]
+fn App() -> impl IntoView {
+    view! {
+        <Html lang="en" dir="ltr" attr:data-theme="light"/>
+
+        // sets the document title
+        <Title text="Welcome to Leptos CSR"/>
+
+        // injects metadata in the <head> of the page
+        <Meta charset="UTF-8"/>
+        <Meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+        <Router>
+            <Routes>
+                <Route path="/" view=Home/>
+                <Route path="/*" view=NotFound/>
+            </Routes>
+        </Router>
+    }
 }
 
 #[component]
-fn App(increment: i32) -> impl IntoView {
-    let (count, set_count) = create_signal(0);
-
+fn Home() -> impl IntoView {
     view! {
-    <div class="container">
-
-            <picture>
-                <source srcset="https://raw.githubusercontent.com/leptos-rs/leptos/main/docs/logos/Leptos_logo_pref_dark_RGB.svg" media="(prefers-color-scheme: dark)" />
-                <img src="https://raw.githubusercontent.com/leptos-rs/leptos/main/docs/logos/Leptos_logo_RGB.svg" alt="Leptos Logo" height="200" width="400"/>
-            </picture>
-
-        <h1>"Testing Leptos"</h1>
-        <h2><i>"On Github Pages..."</i></h2>
-
-        <button
-            on:click= move |_| {
-                set_count(count() + increment)
-            }
-        >
-            "Click me: "
-            {count}
-        </button>
-
-
-    </div>
+        <div class="container">
+            <h1>"Testing Leptos"</h1>
+            <h2><i>"On Github Pages..."</i></h2>
+        </div>
     }
+}
+
+/// "404: Not Found" Page
+#[component]
+pub fn NotFound() -> impl IntoView {
+    view! { <h1>"Uh oh!" <br/> "We couldn't find that page!"</h1>  }
 }
