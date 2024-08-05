9ddd76ffa62561c7e1f0721bd492f4084411beb3
rtthw <172864013+rtthw@users.noreply.github.com>
2024-08-05 13:05:53 -0400

    ...

--- src/main.rs
+++ src/main.rs
@@ -1,64 +1,38 @@
-//! Personal Website
+use leptos::*;
 
-
-
-// ================================================================================================
-
-
-
-#[cfg(not(target_arch = "wasm32"))]
-fn main() -> eframe::Result {
-    eframe::run_native(
-        "rtthw",
-        eframe::NativeOptions {
-            viewport: eframe::egui::ViewportBuilder::default()
-                .with_inner_size([1280.0, 720.0])
-                .with_min_inner_size([1280.0, 720.0]),
-            ..Default::default()
-        },
-        Box::new(|cc| Ok(Box::new(rtthw::App::new(cc)))),
-    )
-}
-
-#[cfg(target_arch = "wasm32")]
 fn main() {
-    let web_options = eframe::WebOptions::default();
-
-    wasm_bindgen_futures::spawn_local(async {
-        let start_result = eframe::WebRunner::new().start(
-            "the_canvas_id",
-            web_options,
-            Box::new(|cc| Ok(Box::new(rtthw::App::new(cc)))),
-        ).await;
-
-        // Remove the loading text and spinner:
-        let loading_text = web_sys::window()
-            .and_then(|w| w.document())
-            .and_then(|d| d.get_element_by_id("loading_text"));
-        if let Some(loading_text) = loading_text {
-            match start_result {
-                Ok(_) => {
-                    loading_text.remove();
-                }
-                Err(e) => {
-                    loading_text.set_inner_html(
-                        "<p> The app has crashed. Sorry, I guess? </p>",
-                    );
-                    panic!("Failed to start eframe: {e:?}");
-                }
-            }
+    mount_to_body(|| {
+        view! {
+            <App increment=5 />
         }
-    });
+    })
 }
 
+#[component]
+fn App(increment: i32) -> impl IntoView {
+    let (count, set_count) = create_signal(0);
+
+    view! {
+    <div class="container">
+
+            <picture>
+                <source srcset="https://raw.githubusercontent.com/leptos-rs/leptos/main/docs/logos/Leptos_logo_pref_dark_RGB.svg" media="(prefers-color-scheme: dark)" />
+                <img src="https://raw.githubusercontent.com/leptos-rs/leptos/main/docs/logos/Leptos_logo_RGB.svg" alt="Leptos Logo" height="200" width="400"/>
+            </picture>
+
+        <h1>"Testing Leptos"</h1>
+        <h2><i>"On Github Pages..."</i></h2>
+
+        <button
+            on:click= move |_| {
+                set_count(count() + increment)
+            }
+        >
+            "Click me: "
+            {count}
+        </button>
 
 
-// ================================================================================================
-
-
-
-
-
-
-
-// ================================================================================================
+    </div>
+    }
+}
