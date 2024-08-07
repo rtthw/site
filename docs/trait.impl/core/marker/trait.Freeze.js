ad724a0ae32de6555ad1dcb09d98a241c0db89b6
rtthw <172864013+rtthw@users.noreply.github.com>
2024-08-07 14:12:18 -0400

    ...

---
+++ docs/trait.impl/core/marker/trait.Freeze.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"rtthw":[["impl Freeze for <a class=\"struct\" href=\"rtthw/struct.Something.html\" title=\"struct rtthw::Something\">Something</a>",1,["rtthw::Something"]]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
