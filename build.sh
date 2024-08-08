d123ff3bc0f12f2058c8fd5d47a527de70ffaa39
rtthw <172864013+rtthw@users.noreply.github.com>
2024-08-08 15:22:17 -0400

    ...

--- build.sh
+++ build.sh
@@ -7,4 +7,4 @@ rm ./docs/rtthw.wasm
 # `wasm-pack build --out-dir docs --target no-modules`
 cargo build -r --lib --target wasm32-unknown-unknown
 cp target/wasm32-unknown-unknown/release/rtthw.wasm docs/rtthw.wasm
-wasm-bindgen docs/rtthw.wasm --out-dir docs
+wasm-bindgen docs/rtthw.wasm --target no-modules --out-dir docs
